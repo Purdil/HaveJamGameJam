5a52ec6efcee9c1f5b0a3b570fcf667e634ad537
dowon <ydowon0119@gmail.com>
2025-12-22 08:32:29 +0900

    Add SaveSystem

---
+++ Assets/01. SO/Events/SaveEventChannel.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using UnityEngine;
+
+namespace _01._SO.Events
+{
+    public enum SaveEventType
+    {
+        Save,
+        Load
+    }
+    [CreateAssetMenu(fileName = "SaveEventChaanel", menuName = "Event/SaveEvent", order = 0)]
+    public class SaveEventChannel : EventChannel<SaveEventType>
+    {
+        
+    }
+}
---
+++ Assets/Core/SaveSystem/ISaveable.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.SaveSystem
+{
+    public interface ISaveable
+    {
+        SaveId SaveId { get; }
+        string GetSaveData();
+        void RestoreData(string loadData);
+    }
+}
