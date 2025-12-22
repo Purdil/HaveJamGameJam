8422d7dcc0b159b94e4b4811abedd52c6736c4a5
BBJ0110 <eeub1290@gmail.com>
2025-12-22 19:21:47 +0900

    룰렛 정보 수정

--- Assets/Member/BBJ/_Script/Roulette/RouletteManager.cs
+++ Assets/Member/BBJ/_Script/Roulette/RouletteManager.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class RouletteManager : MonoBehaviour
@@ -15,21 +16,60 @@ public class RouletteManager : MonoBehaviour
         }
     }
 #endif
+    public void StartRoulette()
+    {
+        // 시작 전 이벤트
+        // 
+    }
 }
 
 public struct RouletNum: IRouletApply, IRouletInfo
 {
-    public int Num1, Num2, Num3;
+    public int? Num1, Num2, Num3;
     public int Priority;
+    public RouletNum(int? num1 = default, int? num2 = default, int? num3 = default, int Priority = default)
+    {
+        this.Num1 = num1;
+        this.Num2 = num2;
+        this.Num3 = num3;
+        this.Priority = Priority;
+    }
+}
+public struct ApplyFinal : IRouletApply
+{
+    public int final;
+    public OperatorSO ApplyOperator;
+    public ApplyFinal(int final = default, OperatorSO applyOperator = default)
+    {
+        this.final = final;
+        this.ApplyOperator = applyOperator;
+    }
 }
 public struct ApplyRouletNum: IRouletApply
 {
-    public RouletNum ApplyNum;
+    public RouletNum RouletNum;
     public OperatorSO ApplyOperator;
+    public ApplyRouletNum(int num1 = default, int num2 = default, int num3 = default, OperatorSO applyOperator = default)
+    {
+        this.RouletNum = new RouletNum(num1, num2, num3);
+        this.ApplyOperator = applyOperator;
+    }
+    public ApplyRouletNum(RouletNum rouletNum, OperatorSO applyOperator = default)
+    {
+        this.RouletNum = rouletNum;
+        this.ApplyOperator = applyOperator;
+    }
 }
 public struct RouletOperator:IRouletApply, IRouletInfo
 {
-    public OperatorSO Operator1, OperatorSO2;
+    public OperatorSO Operator1, Operator2;
+    public int Priority;
+    public RouletOperator(OperatorSO operator1 = default, OperatorSO operator2 = default, int priority = default)
+    {
+        this.Operator1 = operator1;
+        this.Operator2 = operator2;
+        this.Priority = priority;
+    }
 }
 public interface IRouletApply{}
 public interface IRouletInfo {}
