88a92f302d46181f6f92cfcf063190a5b3e7e053
kimdohyun <james49951215@gmail.com>
2025-12-24 02:06:09 +0900

    feat : 페이드인페이드아웃

--- Assets/Member/KDH/Script/EscUi.cs
+++ Assets/Member/KDH/Script/EscUi.cs
@@ -1,33 +1,64 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using csiimnida.CSILib.SoundManager.RunTime;
 using DG.Tweening;
+using Unity.AppUI.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class EscUi : MonoBehaviour
 {
+    [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject tars;
+
+
     public void Countinue()
     {
+        //SoundManager.Instance.PlaySound("Click");
+        SceneManager.sceneLoaded += OnClickButton;
         SceneManager.LoadScene("TitleScene");
-        SoundManager.Instance.PlaySound("Click");
     }
 
     public void OnEnd()
     {
-        Application.Quit();
         SoundManager.Instance.PlaySound("Click");
+        Application.Quit();
     }
     public void GoLobby()
     {
-        SceneManager.LoadScene("LobbyScene");
-        SoundManager.Instance.PlaySound("Click");
+        Debug.Log("[EscUI] ¼Ò¸®!");
+        // SoundManager.Instance.PlaySound("Click");
+        SceneManager.sceneLoaded += OnClickButton;
+        StartCoroutine(GoPlus());
     }
+    public IEnumerator GoPlus()
+    {
+        tars.SetActive(true);
+        yield return new WaitForSecondsRealtime(2f);
+        SceneManager.LoadScene("LobbyScene");
+    }
+
+    public void OnClick()
+    {
+        SoundManager.Instance.PlaySound("Click");
+        panel.SetActive(true);
+    }
+
+    private void OnClickButton(Scene s, LoadSceneMode m)
+    {
+        SoundManager.Instance?.PlaySound("Click");
+        SceneManager.sceneLoaded -= OnClickButton;
+    }
+
     public void GoBattle()
     {
+
+        //SoundManager.Instance.PlaySound("Click");
+        SceneManager.sceneLoaded += OnClickButton;
         SceneManager.LoadScene("CombatScene");
-        SoundManager.Instance.PlaySound("Click");
     }
     public void GoDebt()
     {
