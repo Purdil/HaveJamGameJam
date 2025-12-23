4e0da4012c2d0be39f2682f5cb1d0068cb1321c3
kimdohyun <james49951215@gmail.com>
2025-12-23 23:26:35 +0900

    faet : 다시씬연결

--- Assets/Member/KDH/Script/EscUi.cs
+++ Assets/Member/KDH/Script/EscUi.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using csiimnida.CSILib.SoundManager.RunTime;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,26 +11,32 @@ public class EscUi : MonoBehaviour
     public void Countinue()
     {
         SceneManager.LoadScene("TitleScene");
+        SoundManager.Instance.PlaySound("Click");
     }
 
     public void OnEnd()
     {
         Application.Quit();
+        SoundManager.Instance.PlaySound("Click");
     }
     public void GoLobby()
     {
         SceneManager.LoadScene("LobbyScene");
+        SoundManager.Instance.PlaySound("Click");
     }
     public void GoBattle()
     {
         SceneManager.LoadScene("CombatScene");
+        SoundManager.Instance.PlaySound("Click");
     }
     public void GoDebt()
     {
         //SceneManager.LoadScene("ShopScene");
+        //SoundManager.Instance.PlaySound("Click");
     }
     public void GoShop()
     {
-       // SceneManager.LoadScene("DebtScene");
+        // SceneManager.LoadScene("DebtScene");
+        //SoundManager.Instance.PlaySound("Click");
     }
 }
