5d847d25a0c62ea65d91b75730a53d3c3d20566c
BBJ0110 <eeub1290@gmail.com>
2025-12-23 22:22:12 +0900

    룰렛 버그 수정

--- Assets/Member/BBJ/_Script/SO/Operator/OperatorSO.cs
+++ Assets/Member/BBJ/_Script/SO/Operator/OperatorSO.cs
@@ -3,6 +3,8 @@ public abstract class OperatorSO : ScriptableObject, IOperator<float>
 {
     [field: SerializeField]
     public string std { get; private set; }
+    [field: SerializeField]
+    public int Priority { get; private set; }
     public abstract float Operation(float p1, float p2);
 }
 
