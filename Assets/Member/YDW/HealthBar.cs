a599a596fb6167d3acab7e5a07752b61b3b612dc
dowon <ydowon0119@gmail.com>
2025-12-24 07:49:34 +0900

    Add HealthBar

---
+++ Assets/Member/YDW/HealthBar.cs
@@ -0,0 +1,37 @@
+﻿using Member.YDW.HealthSystem;
+using UnityEngine;
+
+namespace Member.YDW
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Transform healthBar;
+        private AgentHealth healthSystem;
+
+        private void Awake()
+        {
+            healthSystem = transform.parent.GetComponentInChildren<AgentHealth>();
+        }
+
+        private void OnEnable()
+        {
+            UpdateBar(0);
+        }
+        private void Start()
+        {
+            UpdateBar(0);
+            healthSystem.OnDamaged += UpdateBar;
+            healthSystem.OnHealed += UpdateBar;
+        }
+
+        private void UpdateBar(int amount)
+        {
+
+
+            float health = (float)healthSystem.Health / healthSystem.GetMaxHealth();
+            healthBar.localScale = new Vector3(health, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+
+
+        }
+    }
+}
